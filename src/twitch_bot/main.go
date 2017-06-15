a2cff601540d6e99964187b0ddea8d28c116ea50
Chris Girggs <Chris Girggs>
2017-06-14 23:42:37 -0400

    update

--- src/twitch_bot/main.go
+++ src/twitch_bot/main.go
@@ -3,28 +3,24 @@ package main
 import (
 	"fmt"
 	"net"
-//	"os"
+	//	"os"
 	"bufio"
-	"time"
 	"net/textproto"
+	"time"
 )
 
-
 type Bot struct {
-	server string
-	port string
-	nick string
+	server  string
+	port    string
+	nick    string
 	channel string
-	conn net.Conn
-
+	conn    net.Conn
 }
 
 func (bot *Bot) ConsoleInput() {
 	// _ := bufio.NewReader(os.Stdin)
 
-
 }
-
 
 func (bot *Bot) Connect() {
 
@@ -40,20 +36,27 @@ func (bot *Bot) Connect() {
 	fmt.Printf("Connected to IRC server %s\n", bot.server)
 }
 
-
 func (bot *Bot) Chat(message string) {
 	fmt.Fprintf(bot.conn, message+"\r\n")
-	fmt.Printf(message+"\r\n")
+	fmt.Printf(message + "\r\n")
 }
 
 
+
+func ParseMessage(message string) {
+
+
+
+
+}
+
 func main() {
 	bot := &Bot{
-		server	: "irc.twitch.tv",
-		port	: "6667",
-		nick	: "bad_hombres_bot",
-		channel : "bad_hombres",
-		conn	: nil,
+		server:  "irc.twitch.tv",
+		port:    "6667",
+		nick:    "bad_hombres_bot",
+		channel: "bad_hombres",
+		conn:    nil,
 	}
 
 	pass := "oauth:30zdoyl8vntrr7p0g2ub3sx3ferb2r"
@@ -63,8 +66,7 @@ func main() {
 	bot.Chat(fmt.Sprintf("USER %s 8 * :%s", bot.nick, bot.nick))
 	bot.Chat(fmt.Sprintf("PASS %s", pass))
 	bot.Chat(fmt.Sprintf("NICK %s", bot.nick))
-	bot.Chat(fmt.Sprintf("JOIN %s", bot.channel))
-
+	bot.Chat(fmt.Sprintf("JOIN #%s", bot.channel))
 
 	reader := bufio.NewReader(bot.conn)
 	tp := textproto.NewReader(reader)
@@ -76,13 +78,8 @@ func main() {
 			fmt.Printf("ERROR")
 			break
 		}
-		fmt.Printf(line)
+		fmt.Println(line)
 
 	}
 
 }
-
-
-
-
-
